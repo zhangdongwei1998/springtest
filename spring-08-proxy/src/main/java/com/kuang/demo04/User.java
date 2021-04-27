22895e5acacced6df222135fe3c164c17d29e870
zdw <834308083@qq.com>
2021-04-27 10:58:31 +0800

    first update

---
+++ spring-08-proxy/src/main/java/com/kuang/demo04/User.java
@@ -0,0 +1,10 @@
+package com.kuang.demo04;
+
+/**
+ * Author zhangdongwei
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
